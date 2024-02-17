1f91676f8bbaed96ebb54f70a99edffce23b5128
kadir-zeren <k4dzeren@gmail.com>
2024-02-17 13:14:37 +0300

    end

--- index.py
+++ index.py
@@ -1,1 +1,33 @@
-print("Hello")
+print("Hello world")
+print("Cift tirnak yazdim")
+print('''uc tirnak ile yazdim ''')
+print("I'm Great")
+print('"hi" he said')
+print('''"Don't do it" said the manager ''')
+print('''bu birinci satir
+      bu ikinci satir
+      bu uc
+      bu dort''')
+print('3.14')
+print(3.14)
+print('3.14' *3)
+print(3.14*3)
+print(''''We should have enough time for our family "''')
+print('bir')
+print()
+print('uc')
+print()
+print('')
+print()
+print("")
+print('''''')
+print('I\'m great')
+print("\"don't do it\" said the manager")
+print('bu benim cumlem \nbu ikinci satir')
+print('bu benim cumlem \tbu ikinci satir')
+print("ben kofsdkolfksdf;lksd;f lksdl'f sd;\
+      lakdlsakdlaskdlasklda\
+      ;asld;asld;lfkdokfdogfdko")
+print("kullanici ismi") # burada kullanici ismi alindi
+
+x=5 # burada x degeri atandi
